7c13b1ba36bb3b22961acb6005946facf63b6f7c
baijuka <baijuka@yahoo.com>
2021-10-24 15:34:45 +0000

    Add menu links to pages

--- mfdw_root/pages/views.py
+++ mfdw_root/pages/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Page
 
 # Create your views here.
 
 def index(request, pagename):
     pagename = '/'+pagename
-    pg = Page.objects.get(permalink=pagename)
+    pg = get_object_or_404(Page, permalink=pagename)
     context = {
         'title': pg.title,
         'content': pg.bodytext,
         'last_updated': pg.update_date,
+        'page_list':Page.objects.all(),
     }
     # assert False
     return render(request, 'pages/page.html', context)
