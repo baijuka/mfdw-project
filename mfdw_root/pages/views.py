4ead246ce3e459559ecb13bbeee1ccbb22615ffa
baijuka <baijuka@yahoo.com>
2021-10-24 13:37:47 +0000

    add pages to the site

--- mfdw_root/pages/views.py
+++ mfdw_root/pages/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . models import Page
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("<h1>The Meandco Homepage</h1>")
+def index(request, pagename):
+    pagename = '/'+pagename
+    pg = Page.objects.get(permalink=pagename)
+    context = {
+        'title': pg.title,
+        'content': pg.bodytext,
+        'last_updated': pg.update_date,
+    }
+    # assert False
+    return render(request, 'pages/page.html', context)
