9b8da4923e1947deda2347c8ea48b4557110f797
baijuka <baijuka@yahoo.com>
2021-10-28 16:30:47 +0000

    Add functionality to display quotes on logged in user

--- mfdw_root/quotes/views.py
+++ mfdw_root/quotes/views.py
@@ -5,6 +5,9 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from .models import Quote
 from .forms import QuoteForm
@@ -12,9 +15,14 @@ from pages.models import Page
 
 # Create your views here.
 
-class QuoteList(ListView):
-    model = Quote
+class QuoteList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
+    # model = Quote
     context_object_name = 'all_quotes'
+
+    def get_queryset(self):
+        return Quote.objects.filter(
+            username=self.request.user)
 
     def get_context_data(self,**kwargs):
         context = super(QuoteList,self).get_context_data(**kwargs)
@@ -22,12 +30,18 @@ class QuoteList(ListView):
         return context
 
 
+@login_required(login_url=reverse_lazy('login'))
 def quote_req(request):
     submitted = False
     if request.method == 'POST':
         form = QuoteForm(request.POST, request.FILES)
-        if form.is_valid(): 
-            form.save()
+        if form.is_valid():
+            quote = form.save(commit=False) 
+            try:
+                quote.username = request.user
+            except Exception:
+                pass
+            quote.save()
             return HttpResponseRedirect('/quote/?submitted=True')
     else:
         form = QuoteForm()
@@ -37,9 +51,13 @@ def quote_req(request):
     {'form':form, 'page_list':Page.objects.all(), 'submitted':submitted})
 
 
-class QuoteView(DetailView):
-    model = Quote
+class QuoteView(LoginRequiredMixin, DetailView):
+    login_url = reverse_lazy('login')
+    #model = Quote
     context_object_name = 'quote'
+
+    def get_queryset(self):
+        return Quote.objects.filter(username=self.request.user)
 
     def get_context_data(self, **kwargs):
         context = super(QuoteView, self).get_context_data(**kwargs)
